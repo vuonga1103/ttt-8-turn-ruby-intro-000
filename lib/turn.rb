ef5f82c289ebe1049c47c6bb6ba24f0f5bb0426d
Anh Vuong <vuonga1103@yahoo.com>
2020-02-07 19:21:26 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-#displays a tic tac toe board, with board spaces passed as an array
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,53 +6,33 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#coverts a user's place on the board to the index integer
-def input_to_index(user_input)
-  index = user_input.to_i
-  index -= 1
-  return index
+def position_taken?(board, location)
+  board[location] != " "
 end
 
-#checks the number to see if 1) the position is already taken and 2)it is an actual number
-def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
+def valid_move?(board, position)
+  if position.to_i.between?(1,9)
+    if !position_taken?(board, position.to_i-1)
+      true
     end
   end
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
 end
 
-#Takes a users num and the board, and places the X or O char in that position
-def move(board, index, character = "X")
-  board[index] = character
-  return board
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
 end
 
-#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
-def turn (board)
+def turn(board)
   puts "Please enter 1-9:"
-  num = gets.strip()
-  index = input_to_index(num)
-  if valid_move?(board, index) == true
-    move(board, index)
-    display_board(board)
+  input = gets.strip
+  if valid_move?(board, input)
+    move(board, input)
   else
     turn(board)
   end
+  display_board(board)
+end
+
+def position_taken?(board, location)
+  board[location] != " "
 end
